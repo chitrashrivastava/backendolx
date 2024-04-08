4652b10b87ff33188510081ee0d816c0fbe4ec9f
chitrashrivastava <chitrashrivastava64@gmail.com>
2024-04-08 23:03:25 +0530

    add

--- models/config.js
+++ models/config.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://chitrashrivastava64:5qZPjBxQQDA0gyPP@cluster0.lbykw0o.mongodb.net/OLXDB?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://chitrashrivastava64:5u5rtZVYBrZnS7AO@cluster0.lbykw0o.mongodb.net/OLXDB?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>{
     console.log('DB connected')
 })
